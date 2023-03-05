08e06f4d6dec06b410e0b000b549a5a02d42a0c4
sergii13 <sergii.khilchuk13@gmail.com>
2023-03-05 18:14:15 +0200

    first commit

---
+++ config/gulp-tasks/sprite.js
@@ -0,0 +1,42 @@
+import svgSprite from "gulp-svg-sprite";
+export const sprite = () => {
+   return app.gulp.src(`${app.path.src.svgicons}`, {})
+      .pipe(app.plugins.plumber(
+         app.plugins.notify.onError({
+            title: "SVG",
+            message: "Error: <%= error.message %>"
+         }))
+      )
+      .pipe(svgSprite({
+         mode: {
+            symbol: {
+               sprite: '../img/icons/icons.svg',
+               //example: true
+            }
+         },
+         shape: {
+            id: {
+               separator: '',
+            },
+            transform: [
+               {
+                  /*svgo: {
+                     plugins: [
+                        { removeXMLNS: true },
+                        { convertPathData: false },
+                        { removeViewBox: false },
+                     ]
+                  }*/
+               }
+            ]
+         },
+         svg: {
+            rootAttributes: {
+               style: 'display: none;',
+               'aria-hidden': true
+            },
+            xmlDeclaration: false
+         }
+      }))
+      .pipe(app.gulp.dest(`${app.path.srcFolder}`));
+}
